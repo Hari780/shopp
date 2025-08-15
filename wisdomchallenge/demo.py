e7500849cd8000370e41994d6d297a1963961d67
Harish Pawar <115519057+Hari780@users.noreply.github.com>
2025-08-15 19:51:16 +0530

    Update demo.py

--- wisdomchallenge/demo.py
+++ wisdomchallenge/demo.py
@@ -1,30 +1,22 @@
-def sort012(arr):
-    n = len(arr)
-    low = 0
-    mid = 0
-    high = n - 1
+def sort_list(nums):
+    i = 0
+    j = 0
+    k = len(nums) - 1
 
-    while mid <= high:
-        current = arr[mid]
-        
-        if current == 0:
-            temp = arr[low]
-            arr[low] = arr[mid]
-            arr[mid] = temp
-            low += 1
-            mid += 1
-        elif current == 1:
-            mid += 1
+    while j <= k:
+        if nums[j] == 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j += 1
+        elif nums[j] == 1:
+            j += 1
         else:
-            temp = arr[mid]
-            arr[mid] = arr[high]
-            arr[high] = temp
-            high -= 1
-
-    return arr
+            nums[j], nums[k] = nums[k], nums[j]
+            k -= 1
+    return nums
 
 
-examples = [
+ex = [
     [0, 1, 2, 1, 0, 2, 1, 0],
     [2, 2, 2, 2],
     [0, 0, 0, 0],
@@ -32,10 +24,9 @@ examples = [
     [2, 0, 1],
     []
 ]
-for i in range(len(examples)):
-    arr = examples[i]
-    print("Example", i+1)
-    print("Before sorting:", arr)
-    sorted_arr = sort012(arr.copy())
-    print("After sorting :", sorted_arr)
-   print()
+
+for i in range(len(ex)):
+    print("Ex", i+1)
+    print(ex[i])
+    print(sort_list(ex[i].copy()))
+    print()
